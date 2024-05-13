013c23915f9ebb17bdfc523cb30d858f7f1c595d
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-05-13 20:05:07 +0530

    Navbar completed :sparkles: :iphone:

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import Navbar from '@/components/Navbar'
+import React, { ReactNode } from 'react'
 
-function layout() {
-  return (
-    <div>
-      layout
-    </div>
-  )
+function layout({ children }: { children: ReactNode }) {
+    return (
+        <div className=' relative flex h-screen w-full flex-col'>
+            <Navbar/>
+            <div className=" w-full">{children}</div>
+        </div>
+    )
 }
 
 export default layout
