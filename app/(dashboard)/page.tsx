c4b896f0206597463479d2d4a2846ce5e1dd9ac2
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-05-21 00:16:14 +0530

    History section month/year selector made :construction:

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from 'next/navigation'
 import React from 'react'
 import CreateTransactionDialog from './_components/CreateTransactionDialog'
 import Overview from './_components/Overview'
+import History from './_components/History'
 
 async function page() {
   const user = await currentUser()
@@ -36,6 +37,7 @@ async function page() {
         </div>
       </div>
       <Overview userSettings={userSettings}/>
+      <History userSettings={userSettings}/>
     </div>
   )
 }
