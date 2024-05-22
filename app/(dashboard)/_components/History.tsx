68d2df7dc6ce6d32572ad4616cf7aad9fea27827
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-05-22 12:13:59 +0530

    history data api made :construction: :alien:

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -6,6 +6,7 @@ import { Period, TimeFrame } from '@/lib/types'
 import { UserSettings } from '@prisma/client'
 import React, { useMemo, useState } from 'react'
 import HistoryPeriodSelector from './HistoryPeriodSelector'
+import { useQuery } from '@tanstack/react-query'
 
 function History({ userSettings }: { userSettings: UserSettings }) {
     const [timeFrame, setTimeFrame] = useState<TimeFrame>("month")
@@ -15,6 +16,11 @@ function History({ userSettings }: { userSettings: UserSettings }) {
     })
 
     const formatter = useMemo(() => { return GetFormatterForCurrency(userSettings.currency) }, [userSettings.currency])
+    const historyDataQuery=useQuery({
+        queryKey:['overview','history',timeFrame,period],
+        queryFn:()=>fetch(`/api/history-data?timeframe=${timeFrame}&year=${period.year}&month=${period.month}`).then((res)=>res.json())
+    })
+    const dataAvailable=historyDataQuery.data && historyDataQuery.data.length>0
     return (
         <div className=' container'>
             <h2 className=" mt-12 text-3xl font-bold">History</h2>
@@ -22,16 +28,16 @@ function History({ userSettings }: { userSettings: UserSettings }) {
                 <CardHeader className=' gap-2'>
                     <CardTitle className=' grid grid-flow-row justify-between gap-2 md:grid-flow-col'>
                         <HistoryPeriodSelector period={period} setPeriod={setPeriod} timeframe={timeFrame} setTimeframe={setTimeFrame} />
-<div className=" flex h-10 gap-2">
-    <Badge variant={"outline"} className=' flex items-center gap-2 text-sm'>
-        <div className=" h-4 w-4 rounded-full bg-emerald-500"></div>
-        Income
-    </Badge>
-    <Badge variant={"outline"} className=' flex items-center gap-2 text-sm'>
-        <div className=" h-4 w-4 rounded-full bg-red-500"></div>
-        Expense
-    </Badge>
-</div>
+                        <div className=" flex h-10 gap-2">
+                            <Badge variant={"outline"} className=' flex items-center gap-2 text-sm'>
+                                <div className=" h-4 w-4 rounded-full bg-emerald-500"></div>
+                                Income
+                            </Badge>
+                            <Badge variant={"outline"} className=' flex items-center gap-2 text-sm'>
+                                <div className=" h-4 w-4 rounded-full bg-red-500"></div>
+                                Expense
+                            </Badge>
+                        </div>
                     </CardTitle>
                 </CardHeader>
 
